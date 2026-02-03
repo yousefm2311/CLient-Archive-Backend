592d762e98e9f8e659aa69b119494cf8a0f97044
yousef mohamed <yousefmohamed17479191@gmail.com>
2026-02-03 12:10:57 +0200

    2/3/2026

--- src/server.js
+++ src/server.js
@@ -1,18 +1,42 @@
-require('dotenv').config();
-const app = require('./app');
-const { connectDB } = require('./config/db');
+// require('dotenv').config();
+// const app = require('./app');
+// const { connectDB } = require('./config/db');
+
+// const PORT = process.env.PORT || 5000;
+
+// connectDB()
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       // eslint-disable-next-line no-console
+//       console.log(`Server listening on port ${PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     // eslint-disable-next-line no-console
+//     console.error('Failed to connect to MongoDB', err);
+//     process.exit(1);
+//   });
+
+require("dotenv").config();
+const app = require("./app");
+const { connectDB } = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
 connectDB()
   .then(() => {
-    app.listen(PORT, () => {
+    const server = app.listen(PORT, () => {
       // eslint-disable-next-line no-console
       console.log(`Server listening on port ${PORT}`);
     });
+
+    // ✅ مهم للشبكة البطيئة ورفع ملفات
+    server.requestTimeout = 120000; // 2 minutes
+    server.headersTimeout = 125000; // لازم أكبر من requestTimeout
+    server.keepAliveTimeout = 65000;
   })
   .catch((err) => {
     // eslint-disable-next-line no-console
-    console.error('Failed to connect to MongoDB', err);
+    console.error("Failed to connect to MongoDB", err);
     process.exit(1);
   });
